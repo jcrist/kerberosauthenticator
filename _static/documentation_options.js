4f0f98bba1417e82c2ec2d92ca0a3ba292af411e
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-18 16:52:28 +0000

    Update docs after building Travis build 10 of jcrist/kerberosauthenticator
    The docs were built from the branch 'master' against the commit
    210ea59266c7a33b005c13ce30d1b58d5aa13797.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/jcrist/kerberosauthenticator/jobs/521811755.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.14.g22e7b1e',
+    VERSION: '0.1.0+1.g210ea59',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
